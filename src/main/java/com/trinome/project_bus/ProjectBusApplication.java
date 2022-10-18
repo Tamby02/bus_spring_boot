c65c5448d0677d6ba4927a8ae75443aa28e8b72a
Tamby <ihtambyrak@gmail.com>
2022-10-18 13:47:18 +0300

    creation du projet

---
+++ src/main/java/com/trinome/project_bus/ProjectBusApplication.java
@@ -0,0 +1,13 @@
+package com.trinome.project_bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinome/project_bus/ProjectBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinome.project_bus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
